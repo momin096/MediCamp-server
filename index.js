f0ca4d6931c185dfd04c1d8a1dd2f3b243777b90
momin096 <momin09609@gmail.com>
2025-05-15 16:45:24 +0600

    camp delete api created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -77,6 +77,14 @@ async function run() {
         // get all camps
         app.get('/camps', async (req, res) => {
             const result = await campsCollection.find().toArray()
+            res.send(result)
+        })
+
+        // delete a camp 
+        app.delete('/camps/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await campsCollection.deleteOne(query)
             res.send(result)
         })
 
